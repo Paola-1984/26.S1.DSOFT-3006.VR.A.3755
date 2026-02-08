7e065796c581f9ef4630a81cef734e69cd98e6a9
Paola-1984 <rguachamin@estud.tesa.edu.ec>
2026-02-07 22:16:37 -0500

    Inizialización del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mi.primera.aplicacin',
+  appName: 'mi-primera-aplicacin',
+  webDir: 'dist'
+};
+
+export default config;
